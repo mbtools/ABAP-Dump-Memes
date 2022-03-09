969288822d92caef9f33848417d1b27994b48f60
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2022-03-09 20:07:54 +0100

    Update meme-search.js

--- scripts/meme-search.js
+++ scripts/meme-search.js
@@ -1,14 +1,7 @@
-// https://github.com/aDu/meme-search
+// Based on https://github.com/aDu/meme-search
 // NO LICENSE
-const request = require('request')
 
-module.exports = (keyword, ops, cb) => {
-    // Preprocess arguments
-    if (!cb) {
-        cb = ops
-        ops = {}
-    }
-
+function memesearch( keyword, ops ) {
     if (keyword) keyword = keyword.trim()
 
     ops.subreddit = ops.subreddit || 'memes'
@@ -21,26 +14,32 @@ module.exports = (keyword, ops, cb) => {
         `https://www.reddit.com/r/${ops.subreddit}/search.json?q=${keyword}&restrict_sr=1&sort=${ops.sort}`
     )
 
-    request({
-        method: 'GET',
-        uri
-    }, (err, response, body) => {
-        if (err) return cb(err)
-        if (response.status >= 400) return cb(new Error('Status code: ' + response.statusCode))
+    let request = new Request(uri);
 
-        let data = JSON.parse(body)
-
-        let posts = data.data.children
-        let memes = []
+    fetch(request)
+      .then(response => {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          throw new Error('Something went wrong on api server!');
+        }
+      })
+      .then(response => {
+        // console.debug(response);
+        let data = JSON.parse(response.body);
+        let posts = data.data.children;
+        let memes = [];
         
         for (var post of posts) {
-            if (post.data.post_hint != "image") continue // Ignore posts that aren't images
+            if (post.data.post_hint != "image") continue; // Ignore posts that aren't images
             memes.push({
                 title: post.data.title,
                 image_url: post.data.url
-            })
-        }
+            });
+        } 
         
-        cb(null, memes)
-    }).on('error', cb)
+        return memes;
+      }).catch(error => {
+        console.error(error);
+      });
 }
